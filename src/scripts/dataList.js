ca608f57117c942623499c6b8cf7f4872483847a
Wade <158378370+wadevenz@users.noreply.github.com>
2024-08-27 19:34:12 +1000

    feat: html & js skeleton

---
+++ src/scripts/dataList.js
@@ -0,0 +1,17 @@
+let dataArray = [
+    "warthog",
+    "interceptor",
+    "herbie",
+    "lightning mcqueen",
+    "batmobile",
+    "bat tumbler",
+    "cybertruck",
+    "delorean",
+    "elanore",
+    "magic school bus",
+    "toyota corolla"
+];
+
+dataArray.forEach(car => {
+    console.log(car);
+});
