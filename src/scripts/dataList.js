8dc587082afd7be47a0e7ffc7c5ffbdf7f03faa0
Wade <158378370+wadevenz@users.noreply.github.com>
2024-08-27 19:55:27 +1000

    feat: populate page with content

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -12,6 +12,22 @@ let dataArray = [
     "toyota corolla"
 ];
 
+let carsContainerElement = document.getElementById("carsContainer");
+
+let carsContainerList = document.createElement("ul");
+
 dataArray.forEach(car => {
     console.log(car);
+
+    let newCarEntry = document.createElement("li");
+
+    newCarEntry.innerText += car;
+
+    carsContainerList.appendChild(newCarEntry);
 });
+
+carsContainerElement.appendChild(carsContainerList);
+
+
+
+
